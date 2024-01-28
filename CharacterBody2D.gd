7ef4995aa3c35022c696ce58cc8c71b6411d64f2
Yanis Moyon <yanis.moyon@epitech.eu>
2024-01-28 15:54:30 +0100

    ADD: Game scene 2

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 var speed = 600
 var jump_height = -2000
 var gravity = 6000
+var touch_top
+
+func _ready():
+	touch_top = false
+	pass
 
 func _physics_process(delta):
 	# Réinitialiser la vitesse horizontale
@@ -30,6 +35,26 @@ func _physics_process(delta):
 
 	# Déplacer le personnage en utilisant la méthode intégrée de CharacterBody2D
 	move_and_slide()
-
+	
+	for index in get_slide_collision_count():
+		var collision := get_slide_collision(index)
+		var body := collision.get_collider()
+		if body.name == "Fake_Plateform":
+			body.visible = false
+			for child in body.get_children():
+				if child.name == "CollisionShape2D":
+					child.disabled = true
+					touch_top = true
+		if body.name == "Unicorn":
+			# kill player
+			for child in body.get_children():
+				if child.name == "AnimatedSprite2D":
+					child.play("killed_player")
+		if body.name == "Spikes":
+			# kill player
+			pass
+	var invisible = get_node("../Invisible_Floor/CollisionShape2D")
+	if (touch_top == true):
+		invisible.disabled = true
 	if is_on_floor():
 		velocity.y = 0
