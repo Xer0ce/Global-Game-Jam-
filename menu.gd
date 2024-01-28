3542d68ddf2ed4b0d5f7e87c0ac972457a378de4
Felix schrynemaeker <felix.schrynemaekers@epitech.eu>
2024-01-28 10:40:04 +0100

    ADD: menu

---
+++ menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+func _ready():
+
+	# Définir la texture du bouton
+	pass # Replace with function body.
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://player.tscn")
+	
+func _on_settinngs_pressed():
+	get_tree().change_scene_to_file("res://settings.tscn")
+	
+func _on_exit_pressed():
+	get_tree().quit()
